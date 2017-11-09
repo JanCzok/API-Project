2802377e425a50db63389e0a8aa19fc8af9f99df
JanCzok <privat@janczok.de>
2017-11-09 21:14:46 +0100

    R Script with full Google Maps Mining Functionality

--- GoogleMapsDataMiningWithR.R
+++ GoogleMapsDataMiningWithR.R
@@ -3,12 +3,13 @@
 
 library("maps")
 library("googleway")
+library("RMySQL")
 
 #authentification: String Code
 myAPI = "***APIKEY***"
 
+#user is asked to type in a search phrase
 searchPhrase <- readline("Type search phrase: ")
-pageRequest <- as.numeric(readline("Requested amount of pages: "))
 
 searchByName <- function(){
  # search for results matching searchPhrase
@@ -18,12 +19,22 @@ searchByName <- function(){
 	page_token = NULL, simplify = TRUE, myAPI)
 }
 
-searchByLocation <- function(){
+searchByLocation <- function(arg1, arg2, arg3, arg4){
  # search for results matching search phrase in specific location
  # parameter location: specify longitude and latitude
  # parameter radius: specify radius in kilometers
- places = google_places(search_string = NULL, location = c(41.876465, -87.621887), radar = TRUE,
-	radius = 100, rankby = NULL, keyword = NULL, language = NULL,
-	name = searchPhrase, place_type = "store", price_range = NULL, open_now = NULL,
+ places = google_places(search_string = NULL, location = c(arg1, arg2), radar = TRUE,
+	radius = arg3, rankby = NULL, keyword = NULL, language = NULL,
+	name = arg4, place_type = "store", price_range = NULL, open_now = NULL,
 	page_token = NULL, simplify = TRUE, myAPI)
 }
+
+databaseFunction <- function(arg1, arg2, arg3, arg4, arg5) {
+ # saves table on database
+ # arg5 needs to be a data.frame
+ connection = dbConnect(RMySQL::MySQL(), dbname = arg1, username = arg2, password = arg3, host = arg4)
+ dbWriteTable(connection, name = "Google", value = arg5, override = TRUE) 
+}
+
+myResult <- searchByLocation(41.876465, -87.621887, 50000, searchPhrase)
+databaseFunction("Google", 'Me', '***PASSWORD***', 'localhost', ***DATA.FRAME***)
